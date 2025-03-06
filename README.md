def84c46b4efcdf365e9afc6629a68751785b280
ragnaroks <703821945@qq.com>
2025-03-06 23:22:50 +0800

    update

--- README.md
+++ README.md
@@ -1,6 +1,15 @@
 ### usage
-focus on target corpse, press <kbd>CTRL</kbd> and <kbd>P</kbd>
+focus on the target corpse, press <kbd>CTRL</kbd> and <kbd>\\</kbd> when the **Search** interact indicator shown
+
+can change shortcut key via <kbd>F12</kbd> bepinex configuretion panel but can not change <kbd>CTRL</kbd> modifier key
 
 ### compatibility
-- works on SPT@3.10.5
-- works fine with **DynamicMaps** and **Radar**
+- works on SPT@3.10.*
+- should be works on SPT@3.9.*, no tested
+- works on SPT@3.8.3
+- works fine with **DynamicMaps** and **TyrianRadar**
+
+### preview
+|before|after|
+|-|-|
+|![before](./assets/snipaste-20250306T200149Z.jpg)|![after](./assets/snipaste-20250306T202300Z.jpg)|
--- SPTCorpseCleanerMod.cs
+++ SPTCorpseCleanerMod.cs
@@ -1,14 +1,22 @@
 ﻿using BepInEx;
+using BepInEx.Configuration;
 using Comfort.Common;
 using EFT;
 using EFT.Interactive;
+using System;
 using UnityEngine;
 
-
 namespace SPTCorpseCleaner {
+    /// <summary>corpse cleaner Mod for SPTarkov</summary>
     [BepInPlugin("net.skydust.SPTCorpseCleanerPlugin", "SPTCorpseCleanerPlugin", "3.10.5.20250303")]
-    public class SPTCorpseCleanerPlugin : BaseUnityPlugin {
+    [BepInProcess("EscapeFromTarkov")]
+    public class SPTCorpseCleanerPlugin : BaseUnityPlugin {        
+        private Boolean IsBusy{get;set;} = false;
+
+        public ConfigEntry<KeyCode>? ShortcutKey{get;private set;} = null;
+
         protected void Awake () {
+            this.ShortcutKey = this.Config.Bind<KeyCode>("config","shortcut",KeyCode.Backslash,"shortcut, default is the \"\\\", must hold CTRL then press this shortcut key");
             this.Logger.LogDebug("plugin loaded");
         }
 
@@ -16,9 +24,13 @@ namespace SPTCorpseCleaner {
             this.Logger.LogDebug("plugin actived");
         }
 
-        protected void Update () {            
-            if(!Input.GetKey(KeyCode.LeftControl) || !Input.GetKeyUp(KeyCode.P)){return;}
-            this.DeleteCorpse();
+        protected void Update () {
+            if (!Input.GetKey(KeyCode.LeftControl)){return;}
+            KeyCode keyCode = this.ShortcutKey?.Value ?? KeyCode.Backslash;
+            if(!Input.GetKeyUp(keyCode)){return;}
+            if(this.IsBusy){return;}
+            //this.DeleteCorpse();
+            ThreadingHelper.Instance.StartSyncInvoke(this.DeleteCorpse);
         }
 
         protected void OnDestroy() {
@@ -26,21 +38,33 @@ namespace SPTCorpseCleaner {
         }
 
         private void DeleteCorpse () {
-            ClientLocalGameWorld? gw = Singleton<GameWorld>.Instance as ClientLocalGameWorld;
-            if(gw==null || gw.MainPlayer==null || gw.MainPlayer.InteractableObject==null || !gw.MainPlayer.InteractableObject.isActiveAndEnabled){
+            this.IsBusy = true;
+            GameWorld? gameWorld = Singleton<GameWorld>.Instance;
+            if(gameWorld==null){return;}
+            InteractableObject? interactableObject = gameWorld.MainPlayer?.InteractableObject;
+            if(interactableObject==null){
+                this.IsBusy = false;
+                this.Logger.LogWarning("no target");
+                return;
+            }
+            if(!interactableObject.isActiveAndEnabled){
+                this.IsBusy = false;
                 this.Logger.LogWarning("target invalid");
                 return;
             }
-            Corpse? corpse = gw.MainPlayer.InteractableObject.GetComponent<Corpse>();
+            Corpse? corpse = interactableObject.GetComponent<Corpse>();
             if(corpse==null){
-                this.Logger.LogWarning("not a corpse");
+                this.IsBusy = false;
+                this.Logger.LogWarning("target is not a corpse");
                 return;
             }
+            this.IsBusy = false;
             //gw.MainPlayer.CurrentManagedState.OnInventory(false);
             //gw.MainPlayer.CurrentManagedState.Cancel();
             corpse.gameObject.SetActive(false);
             corpse.gameObject.DestroyAllChildren();
-            gw.DestroyLoot(corpse);
+            gameWorld.DestroyLoot(corpse);// Corpse is essentially the IKillableLootItem, pop it for Radar and DynamicMaps
+            this.Logger.LogInfo(String.Concat("corpse \"",interactableObject.name,"\" has been deleted"));
         }
     }
 }
