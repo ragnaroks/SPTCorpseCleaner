bd3d30e9e6f83848bcda600273bb762720014108
ragnaroks <703821945@qq.com>
2025-04-02 13:47:45 +0800

    misc

--- UnityComponents/CorpseNameLabelComponent.cs
+++ UnityComponents/CorpseNameLabelComponent.cs
@@ -27,13 +27,13 @@ namespace SPTCorpseCleaner.UnityComponents {
         public void OnGUI() {
             if(SPTCorpseCleanerPlugin.Debug?.Value!=true){return;}
             if(this.Corpse==null){return;}
-            Vector3 position = Camera.main.WorldToViewportPoint(this.Corpse.transform.position);
+            Vector3 position = Camera.main.WorldToViewportPoint(this.gameObject.transform.position);
             if(position.x>1F || position.x<0F || position.y>1F || position.y<0F || position.z<0F){return;}
             Vector2 positionUI = new Vector2(){
                 x = position.x * Screen.width,
                 y = (1F-position.y) * Screen.height
             };
-            GUI.Label(new Rect(positionUI.x-48F,positionUI.y-12F,96F,24F),String.Concat("<",this.Corpse.name,">"),CorpseNameLabelComponent.GUIStyle);
+            GUI.Label(new Rect(positionUI.x-48F,positionUI.y-12F,96F,24F),String.Concat("[",this.Corpse.name,"]"),CorpseNameLabelComponent.GUIStyle);
         }
 
         public void OnDestroy(){
