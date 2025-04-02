c3085fa7c0c562e0497b00e6a56586655ff32293
ragnaroks <703821945@qq.com>
2025-04-02 17:52:19 +0800

    misc

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 ### usage
-focus on the target corpse, follow the interact indicator
+focus on the target corpse, follow the interact indicator (AKA action menu)
 
 ### compatibility
 check release note for SPT version, works fine with **DynamicMaps** and **TyrianRadar**, but **DynamicMaps** may still shown deleted corpse
+
+you may need this [SPTLootFetching](https://github.com/ragnaroks/SPTLootFetching)
 
 ### preview
 |before|after|
--- SPTCorpseCleanerPlugin.cs
+++ SPTCorpseCleanerPlugin.cs
@@ -1,11 +1,8 @@
 ﻿using BepInEx;
 using BepInEx.Configuration;
-using Comfort.Common;
-using EFT;
 using System;
 
 namespace SPTCorpseCleaner {
-    /// <summary>corpse cleaner Mod for SPTarkov</summary>
     [BepInPlugin("net.skydust.SPTCorpseCleanerPlugin", "SPTCorpseCleanerPlugin", "1.0.3")]
     [BepInProcess("EscapeFromTarkov")]
     public class SPTCorpseCleanerPlugin : BaseUnityPlugin {
@@ -13,22 +10,18 @@ namespace SPTCorpseCleaner {
 
         private AssemblyPatches__GetActionsClass.Smethod9Patch? Smethod9Patche{get;set;} = null;
 
-        private AssemblyPatches_EFT__Interactive__Corpse.AwakePatch? AwakePatch{get;set;} = null;
-
         public static ConfigEntry<Boolean>? Debug{get;private set;} = null;
 
         protected void Awake () {
             SPTCorpseCleanerPlugin.Debug = this.Config.Bind<Boolean>("config","debug",false,"don't touch this");
             //this.Smethod8Patche = new AssemblyPatches__GetActionsClass.Smethod8Patch();
             this.Smethod9Patche = new AssemblyPatches__GetActionsClass.Smethod9Patch();
-            this.AwakePatch = new AssemblyPatches_EFT__Interactive__Corpse.AwakePatch();
             this.Logger.LogDebug("plugin loaded");
         }
 
         protected void Start () {
             //this.Smethod8Patche?.Enable();
             this.Smethod9Patche?.Enable();
-            this.AwakePatch?.Enable();
             this.Logger.LogDebug("plugin actived");
         }
 
@@ -39,7 +32,6 @@ namespace SPTCorpseCleaner {
         protected void OnDestroy() {
             //this.Smethod8Patche?.Disable();
             this.Smethod9Patche?.Disable();
-            this.AwakePatch?.Disable();
             this.Logger.LogDebug("plugin deactived");
         }
     }
