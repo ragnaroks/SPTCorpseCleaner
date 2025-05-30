a3af70a3e2314776d515cf59104ee5f50ab39f49
ragnaroks <703821945@qq.com>
2025-05-31 01:20:39 +0800

    allow search zombie

--- SPTCorpseCleanerPlugin.cs
+++ SPTCorpseCleanerPlugin.cs
@@ -6,7 +6,7 @@ namespace SPTCorpseCleaner {
     [BepInPlugin("net.skydust.SPTCorpseCleanerPlugin", "SPTCorpseCleanerPlugin", "1.0.3")]
     [BepInProcess("EscapeFromTarkov")]
     public class SPTCorpseCleanerPlugin : BaseUnityPlugin {
-        //private AssemblyPatches__GetActionsClass.Smethod8Patch? Smethod8Patche{get;set;} = null;
+        private AssemblyPatches__GetActionsClass.Smethod8Patch? Smethod8Patche{get;set;} = null;
 
         private AssemblyPatches__GetActionsClass.Smethod9Patch? Smethod9Patche{get;set;} = null;
 
@@ -14,13 +14,13 @@ namespace SPTCorpseCleaner {
 
         protected void Awake () {
             SPTCorpseCleanerPlugin.Debug = this.Config.Bind<Boolean>("config","debug",false,"don't touch this");
-            //this.Smethod8Patche = new AssemblyPatches__GetActionsClass.Smethod8Patch();
+            this.Smethod8Patche = new AssemblyPatches__GetActionsClass.Smethod8Patch();
             this.Smethod9Patche = new AssemblyPatches__GetActionsClass.Smethod9Patch();
             this.Logger.LogDebug("plugin loaded");
         }
 
         protected void Start () {
-            //this.Smethod8Patche?.Enable();
+            this.Smethod8Patche?.Enable();
             this.Smethod9Patche?.Enable();
             this.Logger.LogDebug("plugin actived");
         }
@@ -30,7 +30,7 @@ namespace SPTCorpseCleaner {
         }
 
         protected void OnDestroy() {
-            //this.Smethod8Patche?.Disable();
+            this.Smethod8Patche?.Disable();
             this.Smethod9Patche?.Disable();
             this.Logger.LogDebug("plugin deactived");
         }
